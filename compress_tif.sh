4905d54c0b62a4891f7b481a356abec61776cda3
montinevra <23004878+montinevra@users.noreply.github.com>
2023-12-15 14:44:56 -0700

    first real commit

---
+++ compress_tif.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Applies lzw compression to an uncompressed tiff.
+
+for FILE in "$@"; do
+	echo $FILE
+	if [ ! -f "${FILE}" ]; then 
+		echo "	it not file"
+		continue
+	fi
+	echo "	it file"
+	if [[ ! $(exiftool "${FILE}" -filetype) == "File Type                       : TIFF" ]]; then
+		echo "	but it not tif"
+		continue
+	fi
+	echo "	it tif"
+	if [[ ! $(exiftool "${FILE}" -compression) == "Compression                     : Uncompressed" ]]; then
+		echo "	but it not uncompresed"
+		continue
+	fi
+	echo "	it uncompresed"
+	magick -compress lzw "${FILE}" "${FILE}"
+	echo "	but it compresed nao lol"
+done
