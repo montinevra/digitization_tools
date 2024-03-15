3b883be34c1c76d29a51191badeebc0a83b3f6cf
montinevra <23004878+montinevra@users.noreply.github.com>
2024-03-15 13:41:16 -0700

    check if tools are installed

--- compress_tif.sh
+++ compress_tif.sh
@@ -4,6 +4,19 @@
 # just do this:
 # mogrify -auto-orient -compress lzw *.tif
 
+command_exists() {
+	command -v "$1" &> /dev/null
+}
+
+if ! command_exists exiftool; then
+	echo "Exiftool is required but not installed. Aborting."
+	exit 1
+fi
+
+if ! command_exists convert; then
+	echo "ImageMagick is required but not installed. Aborting."
+	exit 1
+fi
 
 for FILE in "$@"; do
 	echo $FILE
@@ -25,3 +38,5 @@ for FILE in "$@"; do
 	convert -auto-orient -compress lzw "${FILE}" "${FILE}"
 	echo "	but it compresed nao lol"
 done
+
+
